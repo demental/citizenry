2f7d697c6732fad04b9891062dcf2d67b5f9f3cc
arnaud sellenet <arnodmental@gmail.com>
2011-10-10 19:20:31 +0200

    fixed stylesheets path issue

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -2,7 +2,7 @@ require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
 require 'fileutils'
-FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets","compiled"))
 
 Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
     :urls => ['/stylesheets'],
