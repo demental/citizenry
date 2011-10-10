38e9b557434c0385805fdec78a255fa40415720d
arnaud sellenet <arnodmental@gmail.com>
2011-10-10 19:12:32 +0200

    compass for heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,9 @@
 require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")
