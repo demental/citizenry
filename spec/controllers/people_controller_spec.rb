5de7b3ed275e33f0bc48174c7d3d801c73ad5938
arnaud sellenet <arnodmental@gmail.com>
2011-10-12 21:16:32 +0200

    added more specs

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -5,28 +5,44 @@ describe PeopleController do
   before { DatabaseCleaner.clean }
 
   describe '#index' do
+    before do
+      @people = []
+      10.times { @people << Factory(:person) }
+    end  
+
+    context "when user is not signed in" do
+      it "does not have delete links for each user" do
+        get :index
+        response.should be_success
+        @people.each do |person|
+          response.body.should_not have_selector("a[href='#{person_path(person)}'][data-method='delete']")
+        end
+      end
+    end
+
     context "when user is signed in" do
       before do
         @user = Factory :user
         sign_in @user
       end
+
       context "when user is not admin" do
+
         it "does not have delete links for each user" do
-          people = []
-          10.times { people << Factory(:person) }
           get :index
           response.should be_success
-          people.each do |person|
+          @people.each do |person|
             response.body.should_not have_selector("a[href='#{person_path(person)}'][data-method='delete']")
           end
         end
       end
-
-      end
+      
+      
       context "when user is admin" do
         before do
           @user.grant!
         end
+
         it "has delete links for each user" do
           people = []
           10.times { people << Factory(:person) }
@@ -36,6 +52,6 @@ describe PeopleController do
           end
         end
       end
-    end
+    end    
   end
 end        
