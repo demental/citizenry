b27e6ca6cf7002fbdda53aa223042d2e4bf11dee
arnaud sellenet <arnodmental@gmail.com>
2012-07-16 00:42:15 +0200

    stylesheets rearranging and enabling sprockets

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,21 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
+
 require 'rack/jsonp'
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  Bundler.require(:default, :assets, Rails.env)
+end
+
 
 module Citizenry
   class Application < Rails::Application
@@ -38,12 +48,18 @@ module Citizenry
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    # config.assets.enbled = true
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    # config.assets.version = '1.0'
+    config.assets.version = '1.0'
+
+    # get precompilation working on heroku:
+    config.assets.initialize_on_precompile = false
 
     # Middleware!
     config.middleware.use Rack::JSONP
+    # Raise exception on mass assignment protection for Active Record models
+    config.active_record.mass_assignment_sanitizer = :strict
+
   end
 end
