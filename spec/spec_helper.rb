1f41d0b3ee8571daeb05ce762c7c81b0d8949f8e
arnaud sellenet <arnodmental@gmail.com>
2012-07-17 10:33:09 +0200

    more accurate webmock for amazon S3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,13 @@ RSpec.configure do |config|
 
   require 'fakeweb'
   FakeWeb.allow_net_connect = false
+  definition = Person.attachment_definitions[:attachment]
+  path = "https*://.*.amazonaws.com/.*"
+  path.gsub!(/:([^\/\.]+)/) do |match|
+      "([^\/\.]+)"
+    end
+  FakeWeb.register_uri(:put, Regexp.new(path), :body => "OK")
+
   require 'faker'
   Faker::Config.locale = :en
 end
